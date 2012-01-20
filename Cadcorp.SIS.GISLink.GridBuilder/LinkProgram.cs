208d1cf56c88db21e548bbbbe8549231c12aba8f
Ben O'Hara <bpo@robotparade.co.uk>
2012-01-20 14:43:10 +0000

    Create GISLink solution and project

---
+++ Cadcorp.SIS.GISLink.GridBuilder/LinkProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace Cadcorp.SIS.GISLink.GridBuilder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Cadcorp.SIS.GisLink.Library;
+    using System.Windows.Forms;
+
+
+    [GisLinkProgram("Build Grid")]
+    public class LinkProgram
+    {
+        private SisApplication sis;
+        public LinkProgram(SisApplication application) 
+        {
+
+        }
+    }
+}
